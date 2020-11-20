0a879699e33715bb5cf838c592ba3832510c77a4
System Administrator <root@Office-Spaces-MacBook-Air.local>
2020-11-20 14:44:54 -0500

    Appointments and Scheduler components merged into /appointments

--- src/app/appointments/appointments.component.ts
+++ src/app/appointments/appointments.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { View } from '@syncfusion/ej2-angular-schedule';
+import { Observable } from 'rxjs';
 import { AuthService } from "../shared/services/auth.service";
 
 @Component({
@@ -8,11 +10,19 @@ import { AuthService } from "../shared/services/auth.service";
   // styleUrls: ['./appointments.component.css']
 })
 export class AppointmentsComponent implements OnInit {
+  availableDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  selectedDay = "Monday";
+  facultyColor = "orange";
+  faculty: Observable<any[]>;
+
   public defaultView : View = 'Month';
 
   constructor(
-    public authService: AuthService
-  ) { }
+    public authService: AuthService,
+    db: AngularFirestore
+  ) {
+    this.faculty = db.collection('faculty').valueChanges();
+  }
 
   ngOnInit(): void {
   }
